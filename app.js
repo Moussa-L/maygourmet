94545ec250477c7b31bd0a9acbde2c25d6527b37
Moussa lidya <moussalidya07@gmail.com>
2026-01-30 09:02:24 +0300

    VIEWS.EJS

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require('express');
 //j'initialise mon application expressJS
 const app = express();
 
+// Configuration du moteur de vue qui doit aller chercher dans le repertoire 'views'
+app.set('views', './views');
+
+// nous utilisons EJS pour les vues
+app.set('view engine', 'ejs');
+
 // API ROUTE pour la page racine : localhost:3004/
 app.get('/', (req, res) => {
     //message à afficher : Bienvenue chez MayGourmet
@@ -14,12 +20,18 @@ res.end();
 app.get('/api/accueil', (req, res) => {
     console.log("hiiiiiiiiii !/api/accueil");
 
-    res.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
+    res.render('acceuil')
+    
+    /*res.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
 
     res.write("<p> Bonjour, je suis à l'accueil </p>");
-    res.end();
+    res.end();*/
 });
 
+app.get('/api/equipe', (req, res) => {
+    console.log("Mon équipe MayGOURMET /api/equipe"); 
+    res.render('equipe') 
+});
 
 /* je define une route qui retourne un message json
 app.use((req, res,next) => {
