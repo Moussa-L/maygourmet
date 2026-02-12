76069f6183c93723bd4832a2a4f957bf4ecc93b4
Moussa lidya <moussalidya07@gmail.com>
2026-02-12 15:51:48 +0300

    .

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 
 //j'ai importer le pilote MYSQL2 utilisé pour interroger la base de données MySQL
-const mysql2 = require("mysql2");
+const mysql2 = require('mysql2');
+
+
+
+//Middleware pour se connecter à la base de données MySQL
+//"pool" est la stratégie de connexion à la base de données 
+const myConnection = require('express-myconnection');
+
 
 //j'initialise mon application expressJS
 const app = express();
@@ -10,15 +17,15 @@ const app = express();
 const optionsConnexionBaseDeDonnees = {
     host: "localhost",
     user:"root",
-    password: "Lidyamousa2907!",
+    password: "Lidyamoussa2907!",
     database: "maygourmet",
     port: 3306
 };
 
+//je dis à mon application expressJS d'utiliser le middleware myConnection pour se connecter à la base de données MySQL
+app.use(myConnection(mysql2, optionsConnexionBaseDeDonnees, "pool"));
 
-//Middleware pour se connecter à la base de données MySQL
-//"pool" est la stratégie de connexion à la base de données 
-const myConnection = require('express-myconnection');
+//const = require('express');
 
 // Configuration du moteur de vue qui doit aller chercher dans le repertoire 'views'
 app.set('views', './views');
@@ -43,7 +50,7 @@ res.end();
 app.get('/api/accueil', (req, res) => {
     console.log("hiiiiiiiiii !/api/accueil");
 
-    res.render('acceuil')
+    res.render('acceuil');
     
     /*res.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
 
@@ -52,10 +59,66 @@ app.get('/api/accueil', (req, res) => {
 });
 
 app.get('/api/equipe', (req, res) => {
-    console.log("Mon équipe MayGOURMET /api/equipe"); 
-    res.render('equipe') 
+    console.log("hiiiiiiiiii !/api/equipe");
+    req.getConnection((erreur, connection) => {
+        if(erreur) {
+            console.log(erreur);
+        } else {
+            connection.query("SELECT * FROM equipe", [], (err, resultatEquipe) => {
+                if (err) {
+                    console.log("Erreur dans la requete SQL ", err);
+                } else {
+                    console.log("Mon équipe : ", resultatEquipe);
+                    res.render("equipe", {resultatEquipe});
+                }
+            });
+
+        }
+    });
+
 });
 
+/*
+app.get('/api/equipe', (req, res) => {
+
+    console.log("Mon équipe MayGOURMET /api/equipe");  
+
+    //je me connecte à la base de données MySQL grace a la methode getConnection()
+    req.getConnection((erreur, connection) => {
+        if(erreur) { //si il y a une erreur de connexion à la base de données, je l'affiche dans le terminal
+            console.log( erreur);
+        } else { //si il n'y a pas d'erreur de connexion à la base de données, je l'affiche dans le terminal
+            connection.query("SELECT * FROM equipe", [], (err, resultatEquipe) => {
+                if (err) {
+                    console.log("Erreur dans la requete SQL ", err);
+                } else {
+                    console.log("Mon équipe : ", resultatEquipe);
+                  
+                }
+});  */   
+
+/*app.get("/api/equipe", (req, res) => {
+    console.log("Mon équipe MayGOURMET /api/equipe");  
+
+    /je me connecte à la base de données MySQL grace a la methode getConnection()
+    req.getConnection((erreur, connection) => {
+        //si il y a une erreur de connexion à la base de données, je l'affiche dans le terminal
+        if(erreur) { 
+            console.log( erreur);
+            //si il n'y a pas d'erreur de connexion à la base de données, je l'affiche dans le terminal
+        } else {
+            //il vas recupérer les membres de l'équipe dans la table "equipe" de la base de données MySQL et les afficher dans le terminal
+            connection.query("SELECT * FROM equipe", [], (err, resultatEquipe) => {
+                if (err) {
+                    console.log("Erreur dans la requete SQL ", err);
+                } else {
+                    console.log("Mon équipe : ", resultatEquipe);
+                }
+            });
+        }
+     
+});*/
+//res.render('equipe');
 /* je define une route qui retourne un message json
 app.use((req, res,next) => {
     res.json({message:"hiiiiiiiiii !"});// envoie une réponse sur json
